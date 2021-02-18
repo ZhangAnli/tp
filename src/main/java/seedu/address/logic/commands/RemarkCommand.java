eeb9d5372a85ef296938ff61daa620e88e20b6b7
ZhangAnli <zhanganli1998@hotmail.com>
2021-02-18 16:17:06 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -36,10 +36,10 @@ public class RemarkCommand extends Command {
      * @param remark of the person to be updated to
      */
     public RemarkCommand(Index index, Remark remark) {
-            requireAllNonNull(index, remark);
+        requireAllNonNull(index, remark);
 
-            this.index = index;
-            this.remark = remark;
+        this.index = index;
+        this.remark = remark;
     }
 
     @Override
